d949b62209bad02771cbec7f620e1f7b66df934f
cvstv <80794140+cvstv@users.noreply.github.com>
2023-07-30 21:18:51 -0700

    uploaded frontend codebase
    user profile file is a static html file to show the UI. other functions will be added in phase 2

---
+++ frontend/SearchBar.js
@@ -0,0 +1,18 @@
+import React, { useState } from 'react';
+
+const SearchBar = ({ onSearch }) => {
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const handleSearch = () => {
+    onSearch(searchTerm);
+  };
+
+  return (
+    <div>
+      <input type="text" value={searchTerm} onChange={(e) => setSearchTerm(e.target.value)} />
+      <button onClick={handleSearch}>Search</button>
+    </div>
+  );
+};
+
+export default SearchBar;
